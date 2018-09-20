3cc666175e67c115fd8d80a0d2e7e37d174b1f50
Claudio Sosa <vicomser.claudio@gmail.com>
2018-09-20 08:39:17 -0300

    clases completas

--- fundamentos Javascript/clase1.js
+++ fundamentos Javascript/clase1.js
@@ -1,26 +1,10 @@
-var nombre = 'Claudio',apellido = 'Sosa'
+var nombre = 'Sacha'
+    var apellido = 'Lifszyc'
 
-var nombreEnMayusculas = nombre.toUpperCase()
-var apellidoEnMinusculas = apellido.toLowerCase()
-var primeraLetraNombre = nombre.charAt(0 )
-var cantidadDeLetrasDelNombre = nombre.length
+    function pasarAMayusculas(nombre) {
+      return nombre.toUpperCase()
+    }
 
-var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
-
-var str = nombre.substr(1,4)
-
-console.log(nombreEnMayusculas);
-console.log(apellidoEnMinusculas);
-console.log(primeraLetraNombre);
-console.log(cantidadDeLetrasDelNombre);
-console.log(nombreCompleto);
-console.log(str);
-console.log(`La ultima Palabra de su nombre es: ${nombre.charAt(nombre.length-1)}`);
-
-//
-// var edad = '28 anos'
-//
-// console.log('Hola: '+nombre+' '+apellido)
-// console.log('Tengo '+ edad);
-//
-// var pesos = 75
+    pasarAMayusculas(nombre + ' ' + apellido)
+    pasarAMayusculas('Lucía')
+    pasarAMayusculas('')
